ac69fb55397bca4b797c20aad78c88cc4ffa2870
Tensibai <tensibai@iabis.net>
2019-04-19 21:13:37 +0000

    Initial test of omnibus patch

---
+++ cinc/omnibus/config/software/chef.rb
@@ -0,0 +1,113 @@
+#
+# Copyright 2012-2019, Chef Software Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+name "chef"
+default_version "master"
+
+license "Apache-2.0"
+license_file "LICENSE"
+
+# Grab accompanying notice file.
+# So that Open4/deep_merge/diff-lcs disclaimers are present in Omnibus LICENSES tree.
+license_file "NOTICE"
+
+# For the specific super-special version "local_source", build the source from
+# the local git checkout. This is what you'd want to occur by default if you
+# just ran omnibus build locally.
+version("local_source") do
+  source path: "#{project.files_path}/../..",
+         # Since we are using the local repo, we try to not copy any files
+         # that are generated in the process of bundle installing omnibus.
+         # If the install steps are well-behaved, this should not matter
+         # since we only perform bundle and gem installs from the
+         # omnibus cache source directory, but we do this regardless
+         # to maintain consistency between what a local build sees and
+         # what a github based build will see.
+         options: { exclude: [ "omnibus/vendor" ] }
+end
+
+# For any version other than "local_source", fetch from github.
+# This is the behavior the transitive omnibus software deps such as chef-dk
+# expect.
+if version != "local_source"
+  source git: "https://github.com/chef/chef.git"
+end
+
+relative_path "chef"
+
+dependency "ruby"
+dependency "rubygems"
+dependency "bundler"
+dependency "ohai"
+dependency "appbundler"
+dependency "libarchive" # for archive resource
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  patch source: "dist.rb.patch", target: "lib/chef/dist.rb"
+  patch source: "chef.gemspec.patch", target: "chef.gemspec"
+
+  # compiled ruby on windows 2k8R2 x86 is having issues compiling
+  # native extensions for pry-byebug so excluding for now
+  excluded_groups = %w{server docgen maintenance pry travis integration ci}
+  excluded_groups << "ruby_prof" if aix?
+  excluded_groups << "ruby_shadow" if aix?
+  excluded_groups << "ed25519" if solaris2?
+
+  # install the whole bundle first
+  bundle "install --without #{excluded_groups.join(' ')}", env: env
+
+  # use the rake install task to build/install chef-config
+  bundle "exec rake install", env: env
+
+  gemspec_name = windows? ? "chef-universal-mingw32.gemspec" : "chef.gemspec"
+
+  # This step will build native components as needed - the event log dll is
+  # generated as part of this step.  This is why we need devkit.
+  gem "build #{gemspec_name}", env: env
+
+  # ensure we put the gems in the right place to get picked up by the publish scripts
+  delete "pkg"
+  mkdir "pkg"
+  copy "chef*.gem", "pkg"
+
+  # the binstubs do not ship in the rubygem, but have to ship in the tests in order to test,
+  # we need to copy those manually over to embedded/bin
+  extra_bin_files = []
+  Dir["#{project_dir}/spec/support/bin/*"].each do |binstub|
+    extra_bin_files << File.basename(binstub)
+    copy binstub, "#{install_dir}/embedded/bin"
+  end
+
+  # restore the binstubs into the ruby gem library location as well
+  block "Install binstubs into the gem" do
+    chef_gem_path = File.expand_path("../..", shellout!("#{install_dir}/embedded/bin/gem which chef").stdout.chomp)
+
+    Dir["#{project_dir}/spec/support/bin/*"].each do |binstub|
+      copy_file binstub, "#{chef_gem_path}/bin"
+    end
+  end
+
+  # Always deploy the powershell modules in the correct place.
+  if windows?
+    mkdir "#{install_dir}/modules/chef"
+    copy "distro/powershell/chef/*", "#{install_dir}/modules/chef"
+  end
+
+  # The extra_bin_files arg is for binstubs we do not ship in the gem
+  appbundle "chef", extra_bin_files: extra_bin_files, env: env
+  appbundle "ohai", env: env
+end
