398d9071f986842e715a346340575ad073a16cbf
Lance Albertson <lance@osuosl.org>
2019-06-12 11:39:23 -0700

    Cleanup install/uninstall on windows

--- scripts/install-cinc.ps1
+++ scripts/install-cinc.ps1
@@ -1,2 +1,14 @@
-$msi = Get-ChildItem -Path *.msi $env:CI_PROJECT_DIR/data/windows/
-Start-Process -FilePath "msiexec.exe" -ArgumentList "/qn /i $msi" -Wait -NoNewWindow
+$ErrorActionPreference = "Stop"
+Write-Host "Finding MSI..."
+$msi = gci -recurse -filter '*.msi' $env:CI_PROJECT_DIR/data/windows/ | select -expand FullName
+Write-Host "Found MSI at $msi, installing..."
+$p = Start-Process -FilePath "msiexec.exe" -ArgumentList "/qn /i $msi" -Passthru -Wait -NoNewWindow
+$p.WaitForExit()
+if ($p.ExitCode -ne 0) {
+    throw "msiexec was not successful. Received exit code $($p.ExitCode)"
+}
+if (Test-Path C:\cc-build) {
+    Write-Host "MSI Installed successfully!"
+} else {
+    throw "MSI was installed however C:\cc-build does not exist"
+}
