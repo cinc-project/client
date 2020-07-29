045974c33689ec3692fff46f93e7e245743f46dc
Tensibai Zhaoying <tensibai@gmail.com>
2020-07-29 22:54:55 +0000

    Fix source and mac tests
    Signed-off-by: Tensibai Zhaoying <tensibai@gmail.com>

--- test/integration/cinc-tests/controls/source_spec.rb
+++ test/integration/cinc-tests/controls/source_spec.rb
@@ -2,7 +2,7 @@ control 'Validate source tarballs' do
   impact 1.0
   title 'Ensure source tarball are correct'
   desc 'Ensure the tarballs match their respective shasum'
-  only_if { ENV['SOURCE_TEST'] == true }
+  only_if { ENV['SOURCE_TEST'] == 'true' }
 
   describe command 'bash -c "cd source/*/cinc/ && sha256sum -c cinc-[0-9]*.tar.xz.sha256sum"' do
     its('exit_status') { should eq 0 }
