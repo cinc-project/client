ecc8b756b7a147ec58f713d1642a2c537293f4f1
Lance Albertson <lance@osuosl.org>
2021-08-27 22:17:07 -0700

    Switch upstream references to main branch
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- patch.sh
+++ patch.sh
@@ -20,7 +20,7 @@
 git_patch() {
   if [ -n "${2}" ] ; then
     CINC_BRANCH="${2}"
-  elif [ "${REF}" == "master" -o -z "${REF}" ] ; then
+  elif [ "${REF}" == "main" -o -z "${REF}" ] ; then
     CINC_BRANCH="stable/cinc"
   else
     CINC_BRANCH="stable/cinc-${REF}"
@@ -36,8 +36,8 @@ set -ex
 # remove any previous builds
 rm -rf chef omnibus-software
 git config --global user.email || git config --global user.email "maintainers@cinc.sh"
-echo "Cloning ${REF:-master} branch from ${ORIGIN:-https://github.com/chef/chef.git}"
-git clone -q -b ${REF:-master} ${ORIGIN:-https://github.com/chef/chef.git}
+echo "Cloning ${REF:-main} branch from ${ORIGIN:-https://github.com/chef/chef.git}"
+git clone -q -b ${REF:-main} ${ORIGIN:-https://github.com/chef/chef.git}
 cd chef
 git_patch chef ${CINC_REF}
 cd omnibus
